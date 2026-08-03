c29a34ecbf5e2e47137ab7003ee6a5d1c9422f07
Dhruv Gola <workdhurubb@gmail.com>
2026-08-03 15:47:47 +0530

    Initial commit

--- components/HeaderActions.tsx
+++ components/HeaderActions.tsx
@@ -1,15 +1,8 @@
-import SlimButton from "./buttons/SlimButton";
 import Hamburger from "./menus/Hamburger";
 
 const HeaderActions = () => {
     return (
         <div className="header-actions flex items-center">
-            <SlimButton 
-                label="Let's Talk"
-                href="/contact-us"
-                ariaLabel="contact us"
-                cls="button--secondary !hidden lg:!inline-flex"
-            />
             <Hamburger />
         </div>
     )
